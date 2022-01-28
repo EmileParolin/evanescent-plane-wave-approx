919644a492c3b3a612afbcfa7f94fb75b83f4577
Emile Parolin <emile.parolin@gmail.com>
2022-01-28 19:33:31 +0100

    Update docs.

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,4 @@
-push!(LOAD_PATH,"../src/")
-using Pkg
-Pkg.activate("./")
+push!(LOAD_PATH,joinpath(@__DIR__, "../src/"))
 using Documenter, Literate, StableApproxEPW
 
 # # Generating some documentation files with `Literate.jl`
@@ -8,4 +6,14 @@ using Documenter, Literate, StableApproxEPW
 # Literate.markdown("../examples/example.jl", "./src/"; flavor=Literate.DocumenterFlavor())
 
 # Generating documentation with `Documenter.jl`
-makedocs(sitename="STrAW Documentation.")
+makedocs(
+    modules = [StableApproxEPW],
+    clean = false,
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    sitename="Stable Approx with EPW",
+    authors="Emile Parolin",
+)
+
+deploydocs(
+    repo = "github.com/EmileParolin/evanescent-plane-wave-approx.git",
+)
